20092dc598fc25b0dc37e1fd7850c8aee4904935
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-04-25 14:44:16 +0800

    add input

--- script/semi-supervised/cora_GCN.sh
+++ script/semi-supervised/cora_GCN.sh
@@ -6,7 +6,7 @@ python ./src/train_new.py \
     --seed 42 \
     --dataset cora \
     --type inceptiongcn \
-    --nhiddenlayer 3 \
+    --nhiddenlayer 15 \
     --nbaseblocklayer 0 \
     --hidden 128 \
     --epoch 400 \
