1177797c30fac71b83ad21df2d4b23dd7f772aae
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-04-25 15:35:13 +0800

    add input

--- src/models.py
+++ src/models.py
@@ -8,6 +8,132 @@ from torch.nn.parameter import Parameter
 device = torch.device("cuda:0")
 
 
+class GCNModel1(nn.Module):
+    """
+       The model for the single kind of deepgcn blocks.
+
+       The model architecture likes:
+       inputlayer(nfeat)--block(nbaselayer, nhid)--...--outputlayer(nclass)--softmax(nclass)
+                           |------  nhidlayer  ----|
+       The total layer is nhidlayer*nbaselayer + 2.
+       All options are configurable.
+    """
+
+    def __init__(self,
+                 nfeat,
+                 nhid,
+                 nclass,
+                 nhidlayer,
+                 dropout,
+                 baseblock="mutigcn",
+                 inputlayer="gcn",
+                 outputlayer="gcn",
+                 nbaselayer=0,
+                 activation=lambda x: x,
+                 withbn=True,
+                 withloop=True,
+                 aggrmethod="add",
+                 mixmode=False):
+        """
+        Initial function.
+        :param nfeat: the input feature dimension.
+        :param nhid:  the hidden feature dimension.
+        :param nclass: the output feature dimension.
+        :param nhidlayer: the number of hidden blocks.
+        :param dropout:  the dropout ratio.
+        :param baseblock: the baseblock type, can be "mutigcn", "resgcn", "densegcn" and "inceptiongcn".
+        :param inputlayer: the input layer type, can be "gcn", "dense", "none".
+        :param outputlayer: the input layer type, can be "gcn", "dense".
+        :param nbaselayer: the number of layers in one hidden block.
+        :param activation: the activation function, default is ReLu.
+        :param withbn: using batch normalization in graph convolution.
+        :param withloop: using self feature modeling in graph convolution.
+        :param aggrmethod: the aggregation function for baseblock, can be "concat" and "add". For "resgcn", the default
+                           is "add", for others the default is "concat".
+        :param mixmode: enable cpu-gpu mix mode. If true, put the inputlayer to cpu.
+        """
+        super(GCNModel1, self).__init__()
+        self.mixmode = mixmode
+        self.dropout = dropout
+
+        if baseblock == "resgcn":
+            self.BASEBLOCK = ResGCNBlock
+        elif baseblock == "densegcn":
+            self.BASEBLOCK = DenseGCNBlock
+        elif baseblock == "mutigcn":
+            self.BASEBLOCK = MultiLayerGCNBlock
+        elif baseblock == "inceptiongcn":
+            self.BASEBLOCK = InecptionGCNBlock
+        else:
+            raise NotImplementedError("Current baseblock %s is not supported." % (baseblock))
+        if inputlayer == "gcn":
+            # input gc
+            self.ingc = GraphConvolutionBS(nfeat, nhid, activation, withbn, withloop)
+            baseblockinput = nhid
+        elif inputlayer == "none":
+            self.ingc = lambda x: x
+            baseblockinput = nfeat
+        else:
+            self.ingc = Dense(nfeat, nhid, activation)
+            baseblockinput = nhid
+
+        outactivation = lambda x: x
+        if outputlayer == "gcn":
+            self.outgc = GraphConvolutionBS(baseblockinput, nclass, outactivation, withbn, withloop)
+        # elif outputlayer ==  "none": #here can not be none
+        #    self.outgc = lambda x: x 
+        else:
+            self.outgc = Dense(nhid, nclass, activation)
+
+        # hidden layer
+        self.midlayer = nn.ModuleList()
+        # Dense is not supported now.
+        # for i in xrange(nhidlayer):
+        for i in range(nhidlayer):
+            gcb = self.BASEBLOCK(in_features=baseblockinput,
+                                 out_features=nhid,
+                                 nbaselayer=nbaselayer,
+                                 withbn=withbn,
+                                 withloop=withloop,
+                                 activation=activation,
+                                 dropout=dropout,
+                                 dense=False,
+                                 aggrmethod=aggrmethod)
+            self.midlayer.append(gcb)
+            baseblockinput = gcb.get_outdim()
+        # output gc
+        outactivation = lambda x: x  # we donot need nonlinear activation here.
+        self.outgc = GraphConvolutionBS(baseblockinput, nclass, outactivation, withbn, withloop)
+
+        self.reset_parameters()
+        if mixmode:
+            self.midlayer = self.midlayer.to(device)
+            self.outgc = self.outgc.to(device)
+
+    def reset_parameters(self):
+        pass
+
+    def forward(self, fea, adj):
+        # input
+        if self.mixmode:
+            x = self.ingc(fea, adj.cpu())
+        else:
+            x = self.ingc(fea, adj)
+
+        x = F.dropout(x, self.dropout, training=self.training)
+        if self.mixmode:
+            x = x.to(device)
+
+        # mid block connections
+        # for i in xrange(len(self.midlayer)):
+        for i in range(len(self.midlayer)):
+            midgc = self.midlayer[i]
+            x = midgc(x, adj)
+        # output, no relu and dropput here.
+        x = self.outgc(x, adj)
+        x = F.log_softmax(x, dim=1)
+        return x
+
 class GCNModel(nn.Module):
     """
        The model for the single kind of deepgcn blocks.
@@ -53,89 +179,39 @@ class GCNModel(nn.Module):
         :param mixmode: enable cpu-gpu mix mode. If true, put the inputlayer to cpu.
         """
         super(GCNModel, self).__init__()
-        self.mixmode = mixmode
+
         self.dropout = dropout
 
-        if baseblock == "resgcn":
-            self.BASEBLOCK = ResGCNBlock
-        elif baseblock == "densegcn":
-            self.BASEBLOCK = DenseGCNBlock
-        elif baseblock == "mutigcn":
-            self.BASEBLOCK = MultiLayerGCNBlock
-        elif baseblock == "inceptiongcn":
-            self.BASEBLOCK = InecptionGCNBlock
-        else:
-            raise NotImplementedError("Current baseblock %s is not supported." % (baseblock))
-        if inputlayer == "gcn":
-            # input gc
-            self.ingc = GraphConvolutionBS(nfeat, nhid, activation, withbn, withloop)
-            baseblockinput = nhid
-        elif inputlayer == "none":
-            self.ingc = lambda x: x
-            baseblockinput = nfeat
-        else:
-            self.ingc = Dense(nfeat, nhid, activation)
-            baseblockinput = nhid
 
-        outactivation = lambda x: x
-        if outputlayer == "gcn":
-            self.outgc = GraphConvolutionBS(baseblockinput, nclass, outactivation, withbn, withloop)
-        # elif outputlayer ==  "none": #here can not be none
-        #    self.outgc = lambda x: x 
-        else:
-            self.outgc = Dense(nhid, nclass, activation)
+        self.ingc = GraphConvolutionBS(nfeat, nhid, activation, withbn, withloop)
 
-        # hidden layer
-        self.midlayer = nn.ModuleList()
-        # Dense is not supported now.
-        # for i in xrange(nhidlayer):
         for i in range(nhidlayer):
-            print('added')
-            gcb = self.BASEBLOCK(in_features=baseblockinput,
-                                 out_features=nhid,
-                                 nbaselayer=nbaselayer,
-                                 withbn=withbn,
-                                 withloop=withloop,
-                                 activation=activation,
-                                 dropout=dropout,
-                                 dense=False,
-                                 aggrmethod=aggrmethod)
+            gcb = GraphConvolutionBS(nhid, nhid, activation, withbn, withloop)
             self.midlayer.append(gcb)
             baseblockinput = gcb.get_outdim()
-        # output gc
+
         outactivation = lambda x: x  # we donot need nonlinear activation here.
         self.outgc = GraphConvolutionBS(baseblockinput, nclass, outactivation, withbn, withloop)
 
-        self.reset_parameters()
-        if mixmode:
-            self.midlayer = self.midlayer.to(device)
-            self.outgc = self.outgc.to(device)
+
 
     def reset_parameters(self):
         pass
 
     def forward(self, fea, adj):
-        # input
-        if self.mixmode:
-            x = self.ingc(fea, adj.cpu())
-        else:
-            x = self.ingc(fea, adj)
+        x = self.ingc(fea, adj)
 
         x = F.dropout(x, self.dropout, training=self.training)
-        if self.mixmode:
-            x = x.to(device)
 
         # mid block connections
         # for i in xrange(len(self.midlayer)):
         for i in range(len(self.midlayer)):
-            print('passed')
             midgc = self.midlayer[i]
             x = midgc(x, adj)
         # output, no relu and dropput here.
         x = self.outgc(x, adj)
         x = F.log_softmax(x, dim=1)
         return x
-
 
 # Modified GCN
 class GCNFlatRes(nn.Module):
