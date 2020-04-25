d13aee5fbb87845f2c6424c1d687f645c1000fff
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-04-25 22:16:19 +0800

    add input

--- src/models.py
+++ src/models.py
@@ -228,7 +228,7 @@ class GCNModel(nn.Module):
         # for i in xrange(len(self.midlayer)):
         for i in range(len(self.midlayer)):
             midgc = self.midlayer[i]
-            x = midgc(x, adj + adj_con)
+            x = midgc(x, adj)
             #x = self.norm(x)
             x = F.dropout(x, self.dropout, training=self.training)
             #vae
@@ -241,7 +241,7 @@ class GCNModel(nn.Module):
             #get masked new adj
             zero_vec = -9e15*torch.ones_like(adj1)
             masked_adj = torch.where(adj > 0, adj1, zero_vec)
-            adj_con =  F.softmax(adj_con + F.softmax(masked_adj, dim=1), dim=1)
+            adj_con =  adj_con + F.softmax(masked_adj, dim=1)
 
 
         # output, no relu and dropput here.
