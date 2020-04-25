41315ff83b467ebd2d79e1171001ec2a25e09cc8
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-04-25 18:40:45 +0800

    add input

--- src/models.py
+++ src/models.py
@@ -242,7 +242,7 @@ class GCNModel(nn.Module):
             #get masked new adj
             zero_vec = -9e15*torch.ones_like(adj1)
             masked_adj = torch.where(adj > 0, adj1, zero_vec)
-            adj_con += F.softmax(masked_adj, dim=1)
+            adj_con = adj_con + F.softmax(masked_adj, dim=1)
             adj = adj + adj_con
 
 
