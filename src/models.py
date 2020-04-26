ae8579ab55d9990692c5062072027e4532649412
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-04-26 14:13:48 +0800

    add input

--- src/models.py
+++ src/models.py
@@ -249,7 +249,7 @@ class GCNModel(nn.Module):
             #get masked new adj
             zero_vec = -9e15*torch.ones_like(adj1)
             masked_adj = torch.where(adj > 0, adj1, zero_vec)
-            adj_con = adj_con + F.softmax(masked_adj, dim=1)
+            adj_con =  F.softmax(adj_con +masked_adj, dim=1)
 
         # output, no relu and dropput here.
         x = self.outgc(torch.cat([x, fea],-1), adj)
