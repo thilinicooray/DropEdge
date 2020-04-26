e380cb94940d7581d7969a48e0a3d62caa0b379a
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-04-26 15:27:19 +0800

    add input

--- src/train_new.py
+++ src/train_new.py
@@ -183,7 +183,7 @@ def train(epoch, train_adj, train_fea, idx_train, val_adj=None, val_fea=None):
         ae_loss = loss_function(preds=recovered, labels=train_adj,
                                 mu=mu, logvar=logvar, n_nodes=train_adj.size(0))
 
-        loss_train = loss_nc + 1.3*ae_loss
+        loss_train = loss_nc + 0.9*ae_loss
         acc_train = accuracy(output[idx_train], labels[idx_train])
 
     loss_train.backward()
