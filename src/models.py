9311fe7890c2f05b8d3fdf56da1a5f450e5a7292
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-04-27 14:27:09 +0800

    add input

--- src/models.py
+++ src/models.py
@@ -308,11 +308,11 @@ class GCNModel_org(nn.Module):
         self.ingc = GraphConvolutionBS(nfeat, nhid, activation, withbn, withloop)
         self.midlayer = nn.ModuleList()
         for i in range(nhidlayer):
-            gcb = GraphConvolutionBS(nhid, nhid, activation, withbn, withloop)
+            gcb = GraphConvolutionBS(nhid+nfeat, nhid, activation, withbn, withloop)
             self.midlayer.append(gcb)
 
         outactivation = lambda x: x  # we donot need nonlinear activation here.
-        self.outgc = GraphConvolutionBS(nhid, nclass, outactivation, withbn, withloop)
+        self.outgc = GraphConvolutionBS(nhid+nfeat, nclass, outactivation, withbn, withloop)
         self.norm = PairNorm()
 
         self.mu = GraphConvolutionBS(nhid, nhid, activation, withbn, withloop)
@@ -341,15 +341,15 @@ class GCNModel_org(nn.Module):
         for i in range(len(self.midlayer)):
             midgc = self.midlayer[i]
 
-            #x = midgc(torch.cat([x, fea],-1), adj)
-            x = midgc(x, adj)
+            x = midgc(torch.cat([x, fea],-1), adj)
+            #x = midgc(x, adj)
             #x = self.norm(x)
             x = F.dropout(x, self.dropout, training=self.training)
             #vae
 
 
         # output, no relu and dropput here.
-        x = self.outgc(x, adj)
+        x = self.outgc(torch.cat([x, fea]), adj)
         x = F.log_softmax(x, dim=1)
         return x
 
