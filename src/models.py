5d7b7cd6d35163e7473c02dd7b6fadc254e8ca1a
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-05-05 22:58:28 +0800

    add input

--- src/models.py
+++ src/models.py
@@ -261,7 +261,7 @@ def attention(query, key, value, mask=None, dropout=None):
              / math.sqrt(d_k)
 
     if mask is not None:
-        scores = scores.masked_fill(mask == 0, -1e9)
+        scores = scores.masked_fill(mask > 0, -1e9)
     p_attn = F.softmax(scores, dim = -1)
     if dropout is not None:
         p_attn = dropout(p_attn)
