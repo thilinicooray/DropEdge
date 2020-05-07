2299389c0a56a9ccad69aebfec922744380b6a4f
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-05-07 11:09:07 +0800

    add input

--- src/models.py
+++ src/models.py
@@ -391,7 +391,7 @@ class GCNModel_org(nn.Module):
             x = F.dropout(x, self.dropout, training=self.training)
             key = midkey(torch.cat([x,fea],-1))
             query = midquery(torch.cat([x,fea],-1))
-            val = val + F.dropout(self.attention(key, query, key, mask), self.dropout, training=self.training)
+            val = val + F.dropout(self.attention(key, query, key, mask), 0.5, training=self.training)
             val_in = val + x
 
 
