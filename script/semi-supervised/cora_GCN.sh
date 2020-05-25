2be294805c7aad5eeeb9a232e010da805297352c
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-05-25 14:46:15 +0800

    add input

--- script/semi-supervised/cora_GCN.sh
+++ script/semi-supervised/cora_GCN.sh
@@ -6,10 +6,10 @@ python ./src/train_new_org.py \
     --seed 42 \
     --dataset cora \
     --type resgcn \
-    --nhiddenlayer 6 \
+    --nhiddenlayer 14 \
     --nbaseblocklayer 0 \
     --hidden 128 \
-    --epoch 315 \
+    --epoch 400 \
     --lr 0.007 \
     --weight_decay 1e-05 \
     --early_stopping 400 \
