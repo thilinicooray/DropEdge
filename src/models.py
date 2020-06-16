abec647c080e7583fb9b3a40f50d469e533dc4e7
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-06-16 15:02:35 +0800

    add input

--- src/models.py
+++ src/models.py
@@ -474,7 +474,7 @@ class GCNModel_org(nn.Module):
                             , local_rep.view(local_rep.size(0), local_rep.size(1), 1))
 
 
-        marginal_rank_loss = torch.mean(torch.max(torch.zeros(org_feat.size(0)).cuda(), non_loc_sim.squeeze() - loc_sim.squeeze() ),0)
+        marginal_rank_loss = torch.mean(torch.max(torch.zeros(org_feat.size(0)).cuda(), margin.squeeze() - non_loc_sim.squeeze() ),0)
 
         return marginal_rank_loss
 
