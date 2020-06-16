447d4e96c9d931f73d8e7b73ebb6d4a6c6bddbbd
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-06-16 19:13:05 +0800

    add input

--- src/models.py
+++ src/models.py
@@ -426,7 +426,7 @@ class GCNModel_org(nn.Module):
 
         val = self.attention(key, self.query_proj(x), key, adj, adj) #what is happening?'''
 
-        #val = self.ingc_g(x, self.get_mask(adj))
+        val = self.ingc_g(x, self.get_mask(adj))
         #val_in = val + x
 
         mask = flag_adj
@@ -447,15 +447,15 @@ class GCNModel_org(nn.Module):
             x = midgc(x, adj)
             x = F.dropout(x, self.dropout, training=self.training)
 
-            #new_val = midgc(x, self.get_mask(mask))
-            #val = val + F.dropout(new_val, self.dropout, training=self.training)
+            new_val = midgc(x, self.get_mask(mask))
+            val = val + F.dropout(new_val, self.dropout, training=self.training)
 
             '''key = midkey(torch.cat([x,fea],-1))
             query = midquery(x)
             val = val + self.attention(key, query, key, adj, mask)'''
-            #mfb_sign_sqrt = torch.sqrt(F.relu(val)) - torch.sqrt(F.relu(-(val)))
+            mfb_sign_sqrt = torch.sqrt(F.relu(val)) - torch.sqrt(F.relu(-(val)))
 
-            #val = F.normalize(mfb_sign_sqrt)
+            val = F.normalize(mfb_sign_sqrt)
             #TODO: gate to decide which amount should come from global and neighbours
 
             #val_in = val + x
@@ -467,8 +467,8 @@ class GCNModel_org(nn.Module):
 
         x = self.outgc(x, adj)
         x = F.log_softmax(x, dim=1)
-        #rank_loss = self.rank_loss(x_enc, last_rep, val)
-        return x, 0.0
+        rank_loss = self.rank_loss(x_enc, last_rep, val)
+        return x, rank_loss
 
     def rank_loss(self, org_feat, local_rep, non_local_rep):
         non_loc_sim = torch.bmm(org_feat.view(org_feat.size(0), 1, org_feat.size(1))
@@ -481,7 +481,7 @@ class GCNModel_org(nn.Module):
                             , local_rep.view(local_rep.size(0), local_rep.size(1), 1))
 
 
-        marginal_rank_loss = torch.mean(torch.max(torch.zeros(org_feat.size(0)).cuda(), margin.squeeze() - non_loc_sim.squeeze() ),0)
+        marginal_rank_loss = torch.mean(torch.max(torch.zeros(org_feat.size(0)).cuda(), margin.squeeze() + non_loc_sim.squeeze() - loc_sim.squeeze() ),0)
 
         return 10*marginal_rank_loss
 
